3ab5d935b340e0c8e642b24106263ea7affdeb1c
jakubcholko23 <jakub.cholko23@my.stjohns.edu>
2026-08-24 16:28:51 -0400

    Changes for render

--- src/main/java/com/gambingapp/gaminghub/security/SecurityConfig.java
+++ src/main/java/com/gambingapp/gaminghub/security/SecurityConfig.java
@@ -45,7 +45,7 @@ public class SecurityConfig{
     @Bean
     public CorsConfigurationSource corsConfigurationSource(){
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:5500", "http://127.0.0.1:5500"));
+        config.setAllowedOrigins(List.of("http://localhost:5500", "http://127.0.0.1:5500", "https://jakub50-coder.github.io"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
